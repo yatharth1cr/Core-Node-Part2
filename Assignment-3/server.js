2296dde15e9c355851f49acdd060910705a3c424
yatharth1cr <yatharthgiri187@gmail.com>
2024-05-19 19:12:53 +0530

    completed few more parts of assignment-3

--- Assignment-3/server.js
+++ Assignment-3/server.js
@@ -1,6 +1,29 @@
 const http = require("http");
+const fs = require("fs");
 
-function handleRequest(req, res) {}
+function handleRequest(req, res) {
+  let store = "";
+  console.log(__dirname + "/users/");
+  // on data the req
+  req.on("data", (chunk) => {
+    store += chunk;
+  });
+
+  // on end the req
+  req.on("end", () => {
+    if (req.method === "POST" && req.url === "/users") {
+      let userName = JSON.parse(store).username;
+      fs.open(__dirname + userName + ".json", "wx", (err, fd) => {
+        if (err) return console.log(err);
+        console.log(fd);
+      });
+      console.log("username: ", userName);
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("page not found");
+    }
+  });
+}
 
 const server = http.createServer(handleRequest);
 
